815a8c0cd7a6b7b0e622bb4b99ddbbbde737680b
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2023-01-16 14:45:20 +0300

    created files to get data from the api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Main from "../Main/Main";
@@ -6,19 +7,35 @@ import Register from "../Register/Register";
 import Login from "../Login/Login";
 import Profile from "../Profile/Profile";
 import NotFound from "../NotFound/NotFound";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import "./App.css";
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [movies, setMovies] = React.useState([]);
+  const [currentUser, setCurrentUser] = React.useState({});
+  const [savedMovies, setSavedMovies] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [checked, setChecked] = React.useState(true);
+
+  const navigation = useNavigate();
+
+  const getUserInfo = () => {
+    
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/saved-movies" element={<SavedMovies />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/signin" element={<Login />} />
-      <Route path="/signup" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <CurrentUserContext.Provider value={currentUser}>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/utils/MainApi.js
@@ -0,0 +1,148 @@
+class MainApi {
+  constructor({ url, headers }) {
+    this._url = url;
+  }
+
+  checkResponseStatus(res) {
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getDataUser() {
+    return fetch(`${this._url}/users/me`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  setDataUser({ name, email }) {
+    return fetch(`${this._url}/users/me`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  getMovies() {
+    return fetch(`${this._url}/movies`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  addMovie(movie) {
+    return fetch(`${this._url}/movies`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        country: movie.country,
+        director: movie.director,
+        duration: movie.duration,
+        year: movie.year,
+        description: movie.description,
+        image: `https://api.nomoreparties.co${movie.image.url}`,
+        trailerLink: movie.trailerLink,
+        thumbnail: `https://api.nomoreparties.co${movie.image.formats.thumbnail.url}`,
+        movieId: movie.id.toString(),
+        nameRU: movie.nameRU,
+        nameEN: movie.nameEN,
+      }),
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  deleteMovie(id) {
+    return fetch(`${this._url}/movies/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  registerUser(name, email, password) {
+    return fetch(`${this._url}/signup`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  userLogin(email, password) {
+    return fetch(`${this._url}/signin`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  getUserData() {
+    return fetch(`${this._url}/users/me`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  updateUserData(name, email) {
+    return fetch(`${this._url}/users/me`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    }).then((res) => this.checkResponseStatus(res));
+  }
+
+  checkToken(token) {
+    return fetch(`${this._url}/users/me`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => this.checkResponseStatus(res));
+  }
+}
+
+const mainApi = new MainApi({
+  url: "https://bac.domainname.diplomryb.nomoredomains.club",
+});
+
+export default mainApi;
