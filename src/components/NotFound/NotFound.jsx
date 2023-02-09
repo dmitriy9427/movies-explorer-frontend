adebb14c7ea744f9a8387f8fc8907cdbe1aa4881
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2023-02-09 14:20:34 +0300

    поправил верстку на страницах регистрации и авторизации

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,13 +5,15 @@ import "./NotFound.scss";
 const NotFound = () => {
   const navigate = useNavigate();
 
-  const back = () => navigate("/");
-
   return (
     <div className="not-found">
       <h5 className="not-found__title">404</h5>
       <p className="not-found__text">Страница не найдена</p>
-      <button type="button" className="not-found__back" onClick={back}>
+      <button
+        type="button"
+        className="not-found__back"
+        onClick={() => navigate(-1)}
+      >
         Назад
       </button>
     </div>
